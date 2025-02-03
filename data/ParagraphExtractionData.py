b304f606c1bae522bbf42f65b6c80e8314cd040a
Gabo <gabriel.piles.glez@gmail.com>
2025-02-03 14:03:13 +0100

    Remove page height width

--- data/ParagraphExtractionData.py
+++ data/ParagraphExtractionData.py
@@ -13,8 +13,6 @@ class XmlData(BaseModel):
     xml_file_name: str
     language: str
     main_language: bool
-    page_width: float
-    page_height: float
     xml_segments_boxes: list[XmlSegmentBox]
 
 
